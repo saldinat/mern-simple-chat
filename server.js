eb9e467eaa2337e1a146686aab4217ae6135395f
saldinat <tomiksa@yandex.ru>
2019-03-04 17:43:16 -0500

    update

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
-//app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.use(express.static(__dirname));
+//app.use(express.static(path.join(__dirname, 'frontend/build')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
